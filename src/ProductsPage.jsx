c62ebe16b17a4f830169243f11ad216cb91057e6
alejandrojuarez117 <aljuarez117@gmail.com>
2025-02-06 17:53:07 -0700

    created Modal.jsx/.css and updated ProductsPage.jsx with imports

--- src/ProductsPage.jsx
+++ src/ProductsPage.jsx
@@ -1,6 +1,9 @@
+import { ProductsIndex } from "./ProductsIndex";
+import { ProductsNew } from './ProductsNew';
+import { ProductsShow } from './ProductsShow';
+import { Modal } from './Modal';
 import axios from 'axios';
 import { useState, useEffect } from "react";
-import { ProductsIndex } from "./ProductsIndex";
 
 export function ProductsPage() {
     const [products, setProducts] = useState([]);
