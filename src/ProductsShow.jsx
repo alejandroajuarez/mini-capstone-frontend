bbe3af765b17d5ff59dcfc30e651ec3a5cf32ea4
alejandrojuarez117 <aljuarez117@gmail.com>
2025-02-12 20:15:34 -0700

    add front end route for update

--- src/ProductsShow.jsx
+++ src/ProductsShow.jsx
@@ -4,7 +4,7 @@ export function ProductsShow({ product, onUpdate, onDestroy }) {
     event.preventDefault()
     console.log("Handling Update");
     const params = new FormData(event.target)
-    ontimeupdate(params, product)
+    onUpdate(params, product)
   }
 
   const handleRemove = () => {
@@ -20,14 +20,30 @@ export function ProductsShow({ product, onUpdate, onDestroy }) {
       <h4><b>Price:</b> ${product.price}</h4>
       <p><b>Image URL:</b> {product.image_url}</p>
       <p><b>Description:</b> {product.description}</p>
+      <p><b>Supplier ID: </b> {product.supplier_id}</p>
 
       <form onSubmit={handleSubmit}>
-        <p>Product Name: <input type="text" name="name"  /></p>
-        <p>Product Supplier ID: <input type="integer" name="supplier_id" /></p>
-        <p>Price: <input type="text" name="price"  /></p>
-        <p>Image URL: <input type="text" name="image_url"  /></p>
-        <p>Description: <input type="text" name="description"  /></p>
-        <button>Update Product</button>
+        <div className="mb-3">
+          <label htmlFor="name" className="form-label">Listing Name: </label>
+          <input type="text" className="form-control" id="name" name="name" />          
+        </div>
+        <div className="mb-3">
+          <label htmlFor="supplier_id" className="form-label">Listing Supplier ID: </label>
+          <input type="integer" className="form-control" id="supplier_id" name="supplier_id" />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="price" className="form-label">Listing Price: </label>
+          <input type="integer" className="form-control" id="price" name="price" />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="image_url" className="form-label">Image URL: </label>
+          <input type="text" className="form-control" id="image_url" name="image_url" />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="description" className="form-label">Listing Description: </label>
+          <input type="text" className="form-control" id="description" name="description" />
+        </div>
+        <button type="submit" className="btn btn-primary">Submit</button>
       </form>
       <br />
       <br />
