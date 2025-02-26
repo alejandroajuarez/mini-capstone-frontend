dd072925c11019c9c98a3fda7979e9023ad51000
alejandrojuarez117 <aljuarez117@gmail.com>
2025-02-25 17:48:32 -0700

    rewrote nav bar

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,4 +1,3 @@
-import { Link } from "react-router-dom";
 import { LogoutLink } from "./LogoutLink";
 
 export function Header() {
@@ -23,15 +22,14 @@ export function Header() {
         <li className="nav-item">
           <a className="nav-link" href="/orders/new">Orders</a>
         </li>
-        <li className="nav-item dropdown">
-          <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Account
-          </a>
-          <ul className="dropdown-menu">
-            <li><Link className="dropdown-item" to="/login">Login</Link></li>
-            <li><Link className="dropdown-item" to="/signup">Sign Up</Link></li>
-            <li><LogoutLink /></li>
-          </ul>
+        <li className="nav-item">
+          <a className="nav-link" href="/login">Login</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="/signup">Sign Up</a>
+        </li>
+        <li>
+          <LogoutLink />
         </li>
       </ul>
     </div>
