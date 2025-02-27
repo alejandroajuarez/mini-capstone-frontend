d2c309f4d0f119bb67cf2cdac4d2294797f0392a
alejandrojuarez117 <aljuarez117@gmail.com>
2025-02-26 20:09:39 -0700

    add search filter function for React

--- src/ProductsIndex.jsx
+++ src/ProductsIndex.jsx
@@ -1,11 +1,16 @@
+import { useState } from "react";
+
 export function ProductsIndex({ products, onShow }) {
+  const [searchFilter, setSearchFilter] = useState("");
   console.log(products);
 
   return (
       <div id="products-index">
+        Search Filter: <input type="text" value={searchFilter} onChange={(event) =>
+          setSearchFilter(event.target.value)} />
         <h1>All Products</h1>
         <div className="row">
-        {products.map((product) => (
+        {products.filter((product) => product.name.toLowerCase().includes(searchFilter.toLowerCase())).map((product) => (
           <div key={product.id} className="col-md-4">
             <div className="card">
               <img src={product.image_url} className="card-img-top" alt="..." height="200px" />
